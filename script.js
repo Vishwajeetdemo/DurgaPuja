b4c363deab13be6960bfb9cc437b473dde349e4c
Vishwajeetdemo <47150216+Vishwajeetdemo@users.noreply.github.com>
2021-09-24 16:45:39 +0530

    Add files via upload
    happy durga puja

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hello");
+ function passValue(){
+       	var firstname=document.getElementById("name").value;
+       	localStorage.setItem("textVal",firstname);
+       	return false;
+       }
